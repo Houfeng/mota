c50618eb606425e61412d4a621721d61e963bfa5
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-03-03 17:50:45 +0800

    dev

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('bootstrap/dist/css/bootstrap.min.css');
+//require('bootstrap/dist/css/bootstrap.min.css');
 require('./assets/common.less');
 
 const mokit = require('mokit');
@@ -6,12 +6,13 @@ const Router = require('mokit-router');
 const Touch = require('mokit-touch');
 const Transition = require('mokit-transition');
 const Frame = require('./pages/frame');
+const Mditor = require('mditor/src/client');
 
 mokit.use(Router);
 mokit.use(Touch);
 mokit.use(Transition);
 
-var router = new mokit.Router();
+let router = new mokit.Router();
 
 router.map({
   '/': '/home',
@@ -19,4 +20,4 @@ router.map({
   '/about': require('./pages/about')
 });
 
-router.start(Frame, document.body);
+router.start(Mditor, document.body);
