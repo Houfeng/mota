b2a1dea9a27bf488e3a5ee3b1fcb6950d0edb9f6
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-03-05 17:24:24 +0800

    dev

--- src/menu/main.js
+++ src/menu/main.js
@@ -1,9 +1,10 @@
 const app = require('electron').app;
+const pkg = require('../../package.json');
 
 module.exports = {
-  label: app.getName(),
+  label: pkg.displayName,
   submenu: [{
-      label: `关于 ${app.getName()}`,
+      label: `关于 ${pkg.displayName}`,
       role: 'about'
     },
     {
