453ddce0d65a9bf7ea2cfcbded9d689f88b31213
Houfeng <admin@xhou.net>
2017-03-06 23:39:04 +0800

    dev

--- bin/release.sh
+++ bin/release.sh
@@ -7,3 +7,6 @@ electron-packager . Mditor --ignore='node_modules' --overwrite --out=release
 cd ./release/Mditor-darwin-x64/Mditor.app/Contents/Resources/app/
 cnpm prune --production
 cnpm i
+cnpm prune --production
+
+#rm -rf ./node_modules/.[!.]*
