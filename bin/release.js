3f5df2caaba6e2d7bae17a03897676bffc319ef4
Houfeng <admin@xhou.net>
2017-03-11 10:15:06 +0800

    重写打包脚本

--- bin/release.js
+++ bin/release.js
@@ -1,10 +1,42 @@
 const path = require('path');
 const fs = require('fs');
+const packager = require('electron-packager');
+const pkg = require('../package');
 
-let pkgFile = path.resolve(__dirname, '../release/Mditor-darwin-x64/Mditor.app/Contents/Resources/app/package.json');
-let buffer = fs.readFileSync(pkgFile);
-let pkg = JSON.parse(buffer.toString());
-delete pkg.devDependencies;
-delete pkg.scripts;
-delete pkg.dev;
-fs.writeFile(pkgFile, JSON.stringify(pkg, null, 2));
+const CWD = path.resolve(__dirname, '../');
+
+//electron-packager . Mditor --ignore='node_modules' --overwrite --out=release  --icon=./design/icon.icns
+packager({
+  name: 'Mditor',
+  appBundleId: 'net.xhou.mditor',
+  appCategoryType: 'public.app-category.utilities',
+  dir: CWD,
+  out: `${CWD}/release`,
+  appVersion: pkg.version,
+  arch: 'x64',
+  icon: `${CWD}/design/icon.icns`,
+  overwrite: true,
+  electronVersion: '1.6.1',
+  ignore: [
+    /node_modules/,
+    /design/,
+    /docs/,
+    /release/,
+    /test/,
+    /bin/,
+    /(.DS_Store|.babelrc|.yml|ignore|.conf.js|.rename|.config.js|jasmine.json)$/
+  ]
+}, function (err, appPaths) {
+  if (err) console.error(err);
+  console.log('packaged', appPaths);
+
+  //处理 deps
+  let pkgFile = path.resolve(__dirname, '../release/Mditor-darwin-x64/Mditor.app/Contents/Resources/app/package.json');
+  let buffer = fs.readFileSync(pkgFile);
+  let pkgObj = JSON.parse(buffer.toString());
+  delete pkgObj.devDependencies;
+  delete pkgObj.scripts;
+  delete pkgObj.dev;
+  fs.writeFile(pkgFile, JSON.stringify(pkgObj, null, 2));
+
+});
--- bin/release.sh
+++ bin/release.sh
@@ -4,18 +4,16 @@ set -e
 
 #icns converter https://iconverticons.com/online/
 
-electron-packager . Mditor --ignore='node_modules' --overwrite --out=release  --icon=./design/icon.icns
-
 node ./bin/release.js
 
-prjroot=$pwd
+#prjroot=$pwd
 
+#install deps
 cd ./release/Mditor-darwin-x64/Mditor.app/Contents/Resources/app/
-
 cnpm prune --production
 cnpm i 
 cnpm prune --production
 
 #cd $prjroot
 #release/Mditor-darwin-x64/Mditor.app
-build --prepackaged ./
+#build --prepackaged ./
