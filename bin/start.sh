867208cfa44013ee848654413d2e5019ac7a5324
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-03-11 16:59:31 +0800

    dev

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-#nokit start --name $npm_package_name -p $npm_package_dev_port --config server -e local
+electron .
--- bin/watch.sh
+++ bin/watch.sh
@@ -5,7 +5,5 @@ set -e
 npm run clear 
 npm run lint
 
-npm run restart
-
 export NODE_ENV=dev 
 webpack --display-error-details --watch
