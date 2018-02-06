b573f9746dca9a641f1a2ff80ed26662018ac9b6
zhanfeng.hzf <admin@xhou.net>
2018-02-06 20:35:31 +0800

    移除无用的构建配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@ const VModule = require('vmodule-webpack-plugin');
 const pkg = require('./package');
 
 module.exports = function (webpackConf) {
-  let babelLoader = Object.assign({}, webpackConf.module.loaders[0]);
-  babelLoader.exclude = [];
-  babelLoader.test = /mokit[\s\S]+\.js$/;
-  webpackConf.module.loaders.push(babelLoader);
   webpackConf.plugins.push(new VModule({
     name: '$info',
     content: { name: pkg.name, version: pkg.version }
