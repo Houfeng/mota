1bff4de90418c8a10cc7a2835262b7e62e37c698
zhanfeng.hzf <admin@xhou.net>
2020-08-21 19:15:49 +0800

    6.3.2

--- src/fitter/render.js
+++ src/fitter/render.js
@@ -4,39 +4,27 @@
  * @author Houfeng <admin@xhou.net>
  */
 
-import React from 'react';
 import { annotation } from '../common/annotation';
 import { convertElement } from './factory';
 import { owner } from './owner';
-
-const originCreateElement = React.createElement;
-React.createElement = function (type, props, ...args) {
-  owner.intercepted = true;
-  if (owner.fitters) {
-    owner.fitters.forEach(fitter => fitter(type, props, owner.model));
-  }
-  return originCreateElement.call(this, type, props, ...args);
-};
 
 function beginRender(component) {
   owner.component = component;
   owner.model = component && component.model;
   owner.fitters = annotation.get('fitters', component);
-  owner.intercepted = false;
 }
 
 function endRender() {
   owner.component = null;
   owner.model = null;
   owner.fitters = null;
-  owner.intercepted = false;
 }
 
 export function wrapRender(initialRender) {
   return function (...args) {
     beginRender(this);
     let element = initialRender.call(this, ...args);
-    if (!owner.intercepted && owner.fitters) {
+    if (owner.fitters) {
       element = convertElement(element, owner.model, owner.fitters);
     }
     endRender();
