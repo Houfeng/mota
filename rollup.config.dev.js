228050580a7d543f0177e098b5618d4975e284e8
zhanfeng.hzf <admin@xhou.net>
2022-04-24 15:31:30 +0800

    调整本地开发配置

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -5,32 +5,39 @@ import resolve from 'rollup-plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
 
-export default {
-  input: './example/index.tsx',
-  output: [
-    {
-      file: './dist/index.js',
-      format: 'iife',
-      sourcemap: true,
-      name: "Mota"
-    }
-  ],
-  external: ['react', 'react-dom'],
-  plugins: [
-    resolve(),
-    commonjs({
-      ignoreDynamicRequires: true,
-      namedExports: {
-        'node_modules/react/index.js': ['useState', 'useMemo', 'useEffect'],
-      },
-    }),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfig: path.resolve(__dirname, './tsconfig.dev.json')
-    }),
-    sourcemaps(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    }),
-  ]
+const createConf = (page) => {
+  return {
+    input: `./examples/${page}.tsx`,
+    output: [
+      {
+        file: `./dist/js/${page}.js`,
+        format: 'iife',
+        sourcemap: true,
+        name: 'Mota'
+      }
+    ],
+    external: ['react', 'react-dom'],
+    plugins: [
+      resolve(),
+      commonjs({
+        ignoreDynamicRequires: true,
+        namedExports: {
+          'node_modules/react/index.js': ['useState', 'useMemo', 'useEffect'],
+        },
+      }),
+      typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfig: path.resolve(__dirname, './tsconfig.dev.json')
+      }),
+      sourcemaps(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }),
+    ]
+  };
 };
+
+export default [
+  createConf('develop'),
+  createConf('benchmark'),
+];
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import cleanup from 'rollup-plugin-cleanup';
 import resolve from 'rollup-plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const createConf = ({ min } = {}) => {
@@ -19,25 +19,25 @@ const createConf = ({ min } = {}) => {
       {
         file: `./dist/mota-umd${suffix}.js`,
         format: 'umd',
-        name: "Mota"
+        name: 'Mota'
       },
       {
         file: `./dist/mota-iife${suffix}.js`,
         format: 'iife',
-        name: "Mota"
+        name: 'Mota'
       }
     ],
     external: ['react', 'react-dom'],
     plugins: [
       resolve(),
       min && terser(),
-      cleanup({ comments: "none" }),
+      cleanup({ comments: 'none' }),
       typescript({
         useTsconfigDeclarationDir: true,
       }),
     ].filter(Boolean)
   };
-}
+};
 
 export default [
   createConf(),
