1c4c0a50945984696259c28c3e299660959178c7
zhanfeng.hzf <admin@xhou.net>
2022-04-25 23:14:38 +0800

    补充 Input 测试用例

---
+++ test/input.test.tsx
@@ -0,0 +1,49 @@
+import { observable, observer } from '../src/';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import assert from 'assert';
+
+const root = document.querySelector('.root');
+
+@observable
+class DemoModel {
+  value = '0';
+}
+
+describe('model', () => {
+
+  it('Input: 输入处理', (done) => {
+    const demo = new DemoModel();
+    const DemoView = observer(({ model }: { model: DemoModel }) => {
+      return (
+        <div>
+          <input
+            id="value"
+            value={model.value}
+            onChange={event => model.value = event.target.value}
+          />
+        </div>
+      );
+    });
+    ReactDOM.render(<DemoView model={demo} />, root);
+    setTimeout(() => {
+      assert.strictEqual(
+        root.querySelector<HTMLInputElement>("#value").value, '0'
+      );
+      window.eval(`
+        const input = document.querySelector("#value");
+        input.value = "1";
+        const event = new Event('input',{bubbles:true,cancelable:true});
+        input.dispatchEvent(event);
+      `);
+      setTimeout(() => {
+        assert.strictEqual(
+          root.querySelector<HTMLInputElement>("#value").value, '1'
+        );
+        done();
+      }, 100);
+    }, 100);
+  });
+
+});
